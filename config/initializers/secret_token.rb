824b4382f7e6eb701af5c9f8bd28759c519a0e3b
Keisuke KITA <kei.kita2501@gmail.com>
2013-03-13 21:43:50 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IshikawaPm25::Application.config.secret_token = 'afa460478a30f533533df6a9096ea3cc603c1dea676963c04039ed77a84e91ff5c3f07e1ad9b147ec8717bf2cb078ef4ba0ce84bb925a755fbdb1b0c6ffd3922'
