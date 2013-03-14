026f8f9a9a18f890cbd5b380db188ab5a3a65852
Keisuke KITA <kei.kita2501@gmail.com>
2013-03-14 14:58:23 +0900

    10件ずつ取得する実装を消去

--- app/models/pm25.rb
+++ app/models/pm25.rb
@@ -6,14 +6,6 @@ class PM25
       month = option[:month].to_s.rjust(2, "0")
       day = option[:day].to_s.rjust(2, "0")
       MyDrip.read_tag(start_key, "collect_pm25_date=#{year}-#{month}-#{day}", 24)
-      #key = start_key
-      #buf = []
-      #while key 
-        #tmp = MyDrip.read_tag(key, "collect_pm25_date=#{year}-#{month}-#{day}", 10)
-        #buf.concat tmp
-        #key = tmp[-1] ? tmp[-1][0] : nil
-      #end
-      #buf
     end
 
     private
